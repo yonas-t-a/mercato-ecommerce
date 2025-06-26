a36b8b664b3f666b4fa888f6b681cc60637b2cd9
yonas-t-a <yonas.tessema@aait.edu.et>
2025-06-26 17:30:37 +0300

    fix the configute the database

---
+++ auth-service/src/models/user.model.js
@@ -0,0 +1,49 @@
+import { createId } from '@paralleldrive/cuid2';
+import sequelize from '../config/db.js';
+import { DataTypes } from 'sequelize';
+import e from 'express';
+
+
+
+const User = sequelize.define('User', {
+    /*
+id (UUID, PK)
+email (string, unique)
+password_hash (string)
+role (enum: 'user' | 'admin')
+reset_token (string, nullable)
+reset_token_expiry (timestamp, nullable)
+created_at (timestamp)
+updated_at (timestamp)
+ */
+    id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        defaultValue: () => createId(),
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password_hash: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    role: {
+        type: DataTypes.ENUM('user', 'admin'),
+        allowNull: false,
+    },
+    reset_token: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    reset_token_expiry: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    }
+},{
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+})
